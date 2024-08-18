69be50e81e69ad1ae89d51dcf9bd02db7640883d
Kingsley Boateng <geniusboateng@gmail.com>
2024-08-18 21:14:02 +0000

    modify MainContent.jsx

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -4,6 +4,7 @@ function MainContent(props) {
   return (
     <main style={{ padding: '20px', margin: '20px auto', backgroundColor: '#f0f8ff', borderRadius: '8px', width: '80%', textAlign: 'justify' }}>
       {props.children}
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
