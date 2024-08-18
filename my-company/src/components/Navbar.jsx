1257c8411b1e5b6f03f1f670e394749288d49750
Kingsley Boateng <geniusboateng@gmail.com>
2024-08-18 23:07:20 +0000

    add my-company directory

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{ padding: '10px', backgroundColor: '#333', color: 'white', textAlign: 'center' }}>
+      <Link to="/" style={{ color: 'white', margin: '0 15px', textDecoration: 'none' }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ color: 'white', margin: '0 15px', textDecoration: 'none' }}>
+        About
+      </Link>
+      <Link to="/services" style={{ color: 'white', margin: '0 15px', textDecoration: 'none' }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ color: 'white', margin: '0 15px', textDecoration: 'none' }}>
+        Contact
+      </Link>
+    </nav>
+  );
+}
+
+export default Navbar;
