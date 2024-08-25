f1677e2a8c10ec9b6db10c547cd44cf492d5b547
Kingsley Boateng <geniusboateng@gmail.com>
2024-08-25 22:50:16 +0000

    modify src components

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetails from './components/RecipeDetails';
+import RecipeDetailsWrapper from './components/RecipeDetailsWrapper';
+import SearchBar from './components/SearchBar';
 
-function App() {
+const App = () => {
   return (
     <Router>
       <div>
-        <h1>Recipe Sharing App</h1>
+        <SearchBar />
         <Routes>
           <Route path="/" element={<RecipeList />} />
           <Route path="/add" element={<AddRecipeForm />} />
@@ -16,11 +18,6 @@ function App() {
       </div>
     </Router>
   );
-}
-
-const RecipeDetailsWrapper = () => {
-  const { recipeId } = useParams();
-  return <RecipeDetails recipeId={parseInt(recipeId)} />;
 };
 
 export default App;
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,20 +1,17 @@
+import React from 'react';
 import { useRecipeStore } from '../recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);
 
   return (
     <div>
-      {recipes.length > 0 ? (
-        recipes.map((recipe) => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))
-      ) : (
-        <p>No recipes available. Please add one!</p>
-      )}
+      {filteredRecipes.map((recipe) => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
     </div>
   );
 };
