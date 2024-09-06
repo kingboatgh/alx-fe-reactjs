f88dfaffc1fe447d1a5eff03d31d48007d05dc4b
Kingsley Boateng <geniusboateng@gmail.com>
2024-09-06 13:22:47 +0000

    add tailwind-react-integration/

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+      <p className="mt-4 text-lg">Tailwind CSS is now integrated!</p>
+    </div>
+  );
+}
+
+export default App;
