b05224800c6d642000867a8d242b1b18d83534b5
Kingsley Boateng <geniusboateng@gmail.com>
2024-09-06 13:32:48 +0000

    modify tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Include your source files
+    "./public/index.html",        // Include your HTML files
   ],
   theme: {
     extend: {},
   },
+  darkMode: 'media', // or 'class' if you want manual control
   plugins: [],
 };
