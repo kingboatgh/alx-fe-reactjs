53ff3c8280d61cc662c7b5b0e54b7ec5e5c67705
Kingsley Boateng <geniusboateng@gmail.com>
2024-09-08 23:36:12 +0000

    modify

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,6 +4,7 @@ export default UserProfile;
 
 
 
+
 function UserProfile() {
   return (
     <div className="bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm mx-auto my-10 md:my-20 rounded-lg shadow-lg">
